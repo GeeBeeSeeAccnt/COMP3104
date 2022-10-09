03afbd257d94925d3eafb43ccaf1dc636f5f6c5e
KnowBodyNew <wendellkeith.salting.321@outlook.com>
2022-10-09 13:19:09 -0400

    Github action script updated

---
+++ build/precache-manifest.1ed1cee25c2134cde45fe218473535ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc53980d8186f0a1204",
+    "url": "/KnowBodyNew/COMP3104/static/css/main.ab93bcd3.chunk.css"
+  },
+  {
+    "revision": "cfc53980d8186f0a1204",
+    "url": "/KnowBodyNew/COMP3104/static/js/main.cfc53980.chunk.js"
+  },
+  {
+    "revision": "f4cb20037c5f7120e096",
+    "url": "/KnowBodyNew/COMP3104/static/js/runtime~main.f4cb2003.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/KnowBodyNew/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "bc37ab5b47202463c89380f7688eba61",
+    "url": "/KnowBodyNew/COMP3104/index.html"
+  }
+];
